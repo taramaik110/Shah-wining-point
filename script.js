9bfb7ac5c2c8d3aaa4a56d18f145aa0fc1089246
taramaik110 <taramalik030@gmail.com>
2025-06-15 13:55:15 +0500

    All programs
    Signed-off-by: taramaik110 <taramalik030@gmail.com>

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('Game loaded');
+
+function enterGame() {
+  const username = document.getElementById('username').value;
+  if (username.trim()) {
+    document.getElementById('loginPage').style.display = 'none';
+    document.getElementById('gamePage').style.display = 'block';
+    document.getElementById('greeting').innerText = 'Hi ' + username + '! Select a Box';
+    createBoxes();
+    document.getElementById('bgMusic').play();
+  }
+}
+
+function createBoxes() {
+  const boxContainer = document.getElementById('boxContainer');
+  for (let i = 1; i <= 100; i++) {
+    const box = document.createElement('div');
+    box.className = 'box';
+    box.innerText = i;
+    box.onclick = () => selectBox(i);
+    boxContainer.appendChild(box);
+  }
+}
+
+let boxSelected = false;
+
+function selectBox(num) {
+  if (boxSelected) return;
+  boxSelected = true;
+  document.getElementById('message').innerText = 'You selected Box #' + num + '. Best of Luck Tonight! Winner will be announced at 7 PM.';
+}
